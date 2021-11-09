24f7b6a9b46d3cc35651109621c4898b530a0e33
Piotr Kupis <piotr.kupis42@gmail.com>
2021-11-09 12:46:38 +0100

    add returning XML analysis

--- src/main/java/com/example/api/ApiController.java
+++ src/main/java/com/example/api/ApiController.java
@@ -1,5 +1,7 @@
 package com.example.api;
 
+import org.json.JSONObject;
+import org.json.XML;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +16,16 @@ public class ApiController {
     public String analyze(@PathVariable("text") String text,
         @PathVariable("format") String format) {
 
+        RestTemplate restTemplate = new RestTemplate();
+
         switch (format) {
             case "JSON":
-                RestTemplate restTemplate = new RestTemplate();
                 return restTemplate.getForObject(API + text, String.class);
+            case "XML":
+                JSONObject jsoObject = new JSONObject(
+                    restTemplate.getForObject(API + text, String.class));
+                String xml = XML.toString(jsoObject);
+                return "<analyze>" + xml + "</analyze>";
             default:
                 return "Incorrect format";
         }
